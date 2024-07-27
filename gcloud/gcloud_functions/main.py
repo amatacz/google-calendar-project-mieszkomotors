16015d671b617451ad04d7f8fda39a8d28e7a80f
aleksandramatacz <matacz.am@pg.com>
2024-07-27 20:40:03 +0200

    source file url corrected

--- gcloud/gcloud_functions/main.py
+++ gcloud/gcloud_functions/main.py
@@ -28,7 +28,7 @@ def create_new_events(request, context=None):
     # Get file, transform it and get list of events to be created
     # source_file_url = GoogleServiceIntegratorObject.get_source_file_url()
     # source_file = DataTransformerObject.load_source_file_from_gdrive(source_file_url)
-    source_file = DataTransformerObject.load_source_file_from_gdrive("https://docs.google.com/spreadsheets/d/1GvAwkCIMPoSAxbfsCbLblEVp0E5CeFw6//export?format=xlsx")
+    source_file = DataTransformerObject.load_source_file_from_gdrive("https://docs.google.com/spreadsheets/d/1GvAwkCIMPoSAxbfsCbLblEVp0E5CeFw6/export?format=xlsx")
     source_file_transformed = DataTransformerObject.transform_file(source_file)
     events_to_be_created = DataTransformerObject.get_dict_of_events_from_timeframe(source_file_transformed, START, END)
         
