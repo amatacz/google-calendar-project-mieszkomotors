b3d31fdad0787d2add995dd5ea0639aced3acc39
aleksandramatacz <matacz.am@pg.com>
2024-07-27 20:01:15 +0200

    logs folder moved to the gcloud/gcloud_functions

--- gcloud/gcloud_functions/main.py
+++ gcloud/gcloud_functions/main.py
@@ -1,7 +1,7 @@
 from utils.google_integration import GoogleServiceIntegrator
 from utils.data_transformation import DataTransformer
 from utils.utils import UtilsConfigurator
-from utils.logger_config import setup_logging
+# from utils.logger_config import setup_logging
 import functions_framework
 
 
@@ -9,8 +9,9 @@ import functions_framework
 
 @functions_framework.http
 def create_new_events(request, context=None):
-    logger = setup_logging()
-    logger.info("Starting main execution.")
+    # logger = setup_logging()
+    # logger.info("Starting main execution.")
+    print("Starting main execution.")
 
     # Create google services
     GoogleServiceIntegratorObject = GoogleServiceIntegrator()
@@ -31,25 +32,29 @@ def create_new_events(request, context=None):
     events_to_be_created = DataTransformerObject.get_dict_of_events_from_timeframe(source_file_transformed, START, END)
         
     if not events_to_be_created:
-        logger.info("No upcoming events - cannot proceed with events creation. Exiting...")
+        # logger.info("No upcoming events - cannot proceed with events creation. Exiting...")
+        print("No upcoming events - cannot proceed with events creation. Exiting...")
         return None
     else:
         # Get follow up events from given timeframe
         existing_next_month_follow_up_events = GoogleServiceIntegratorObject.get_follow_up_events_list(START, END)
 
         if not existing_next_month_follow_up_events:
-            logger.info("No existing events for following moth. Proceed with events creation.")
+            # logger.info("No existing events for following moth. Proceed with events creation.")
+            print("No existing events for following moth. Proceed with events creation.")
             try:
                 for event in events_to_be_created.values():
                     GoogleServiceIntegratorObject.create_follow_up_events(event)
             except Exception as e:
-                logger.error(f"Error while creating events {e}")
+                # logger.error(f"Error while creating events {e}")
+                print(f"Error while creating events {e}")
         else:
             for event_to_be_created in events_to_be_created.values():
                 if GoogleServiceIntegratorObject.validate_if_event_already_exists_in_calendar(existing_next_month_follow_up_events, event_to_be_created):
                     # Create events in Google Calendar if event is not present in calendar
                     GoogleServiceIntegratorObject.create_follow_up_events(event_to_be_created)
-            logger.info("Process finished successfully.")
+            # logger.info("Process finished successfully.")
+            print("Process finished successfully.")
 
     return "Events creation function finished"
 
