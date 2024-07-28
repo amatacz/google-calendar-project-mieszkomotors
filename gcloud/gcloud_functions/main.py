9c66fd43efb87e6001c15b5e291e2b1fa8d4c654
aleksandramatacz <matacz.am@pg.com>
2024-07-28 05:31:48 +0200

    google logging added

--- gcloud/gcloud_functions/main.py
+++ gcloud/gcloud_functions/main.py
@@ -1,7 +1,7 @@
 from utils.google_integration import GoogleServiceIntegrator
 from utils.data_transformation import DataTransformer
 from utils.utils import UtilsConfigurator
-# from utils.logger_config import setup_logging
+from utils.logger_config import setup_logging
 import functions_framework
 import os
 
@@ -9,12 +9,16 @@ import os
 
 @functions_framework.http
 def create_new_events(request, context=None):
-    # logger = setup_logging()
-    # logger.info("Starting main execution.")
+    logger = setup_logging()
+    logger.log_text("Starting main execution.")
+
     print("Starting main execution.")
 
+    # Get env var
     SOURCE_FILE_URL = os.getenv("SOURCE_FILE_URL")
-
+    if not SOURCE_FILE_URL:
+        return "Environment variable SOURCE_FILE_URL is not set.", 400
+    
     # Create google services
     GoogleServiceIntegratorObject = GoogleServiceIntegrator()
     GoogleServiceIntegratorObject.get_google_services()
@@ -28,35 +32,33 @@ def create_new_events(request, context=None):
 
 
     # Get file, transform it and get list of events to be created
-    # source_file_url = GoogleServiceIntegratorObject.get_source_file_url()
     source_file = DataTransformerObject.load_source_file_from_gdrive(SOURCE_FILE_URL)
-    # source_file = DataTransformerObject.load_source_file_from_gdrive("https://docs.google.com/spreadsheets/d/1GvAwkCIMPoSAxbfsCbLblEVp0E5CeFw6/export?format=xlsx")
     source_file_transformed = DataTransformerObject.transform_file(source_file)
     events_to_be_created = DataTransformerObject.get_dict_of_events_from_timeframe(source_file_transformed, START, END)
         
     if not events_to_be_created:
-        # logger.info("No upcoming events - cannot proceed with events creation. Exiting...")
-        print("No upcoming events - cannot proceed with events creation. Exiting...")
+        logger.log_text("No upcoming events - cannot proceed with events creation. Exiting...")
+        # print("No upcoming events - cannot proceed with events creation. Exiting...")
         return "No upcoming events - cannot proceed with events creation. Exiting..."
     else:
         # Get follow up events from given timeframe
         existing_next_month_follow_up_events = GoogleServiceIntegratorObject.get_follow_up_events_list(START, END)
 
         if not existing_next_month_follow_up_events:
-            # logger.info("No existing events for following moth. Proceed with events creation.")
+            logger.log_text("No existing events for following moth. Proceed with events creation.")
             print("No existing events for following moth. Proceed with events creation.")
             try:
                 for event in events_to_be_created.values():
                     GoogleServiceIntegratorObject.create_follow_up_events(event)
             except Exception as e:
-                # logger.error(f"Error while creating events {e}")
+                logger.log_text(f"Error while creating events {e}")
                 print(f"Error while creating events {e}")
         else:
             for event_to_be_created in events_to_be_created.values():
                 if GoogleServiceIntegratorObject.validate_if_event_already_exists_in_calendar(existing_next_month_follow_up_events, event_to_be_created):
                     # Create events in Google Calendar if event is not present in calendar
                     GoogleServiceIntegratorObject.create_follow_up_events(event_to_be_created)
-            # logger.info("Process finished successfully.")
+            logger.log_text("Process finished successfully.")
             print("Process finished successfully.")
 
     return "Events creation function finished"
--- gcloud/gcloud_functions/utils/logger_config.py
+++ gcloud/gcloud_functions/utils/logger_config.py
@@ -1,24 +1,27 @@
-import logging
-from logging.handlers import RotatingFileHandler
+# import logging
+# from logging.handlers import RotatingFileHandler
+
+from google.cloud import logging
+
 
 def setup_logging():
-    logger = logging.getLogger("google_events_logger")
-    logger.setLevel(logging.DEBUG)
+    logger = logging.Client()
+    logger.logger("google_events_logger")
 
-    # Create handlers for displaying msgs in console (c_handler) and write them to the file (f_handler)
-    c_handler = logging.StreamHandler()
-    f_handler = RotatingFileHandler('../logs/google_events_logger.log', maxBytes=2000, backupCount=5, encoding='utf-8')
-    c_handler.setLevel(logging.WARNING)
-    f_handler.setLevel(logging.DEBUG)
+    # # Create handlers for displaying msgs in console (c_handler) and write them to the file (f_handler)
+    # c_handler = logging.StreamHandler()
+    # f_handler = RotatingFileHandler('../logs/google_events_logger.log', maxBytes=2000, backupCount=5, encoding='utf-8')
+    # c_handler.setLevel(logging.WARNING)
+    # f_handler.setLevel(logging.DEBUG)
 
-    # Create formatters and add them to handlers
-    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    c_handler.setFormatter(c_format)
-    f_handler.setFormatter(f_format)
+    # # Create formatters and add them to handlers
+    # c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    # f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # c_handler.setFormatter(c_format)
+    # f_handler.setFormatter(f_format)
 
-    # Add handlers to the logger
-    logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
+    # # Add handlers to the logger
+    # logger.addHandler(c_handler)
+    # logger.addHandler(f_handler)
 
     return logger
